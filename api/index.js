012f6b14a515b99bb688e791fb26491b397e63c6
jaimemendozadev <jaimemendozadev@gmail.com>
2018-09-09 20:41:34 -0700

    Add middleware function

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const middleware = require('./middleware');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Hit the API!</h1>'));
+middleware(app);
+
+app.get("/", (req, res) => res.send("<h1>Hit the API!</h1>"));
 
 module.exports = app;

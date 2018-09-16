787f64a7e006f04669564d01be0466a8ec3a888c
jaimemendozadev <jaimemendozadev@gmail.com>
2018-09-16 07:56:05 -0700

    Add skeleton User mutations and queries

---
+++ api/services/graphql/User/mutations/index.js
@@ -0,0 +1,13 @@
+// parent, args, context, info
+
+const createUser = () => {}
+
+const updateUser = () => {}
+
+const addSignature = () => {}
+
+module.exports = {
+  createUser,
+  updateUser,
+  addSignature,
+}
---
+++ api/services/graphql/User/queries/index.js
@@ -0,0 +1,6 @@
+// parent, args, context, info
+const getUser = () => {}
+
+module.exports = {
+  getUser,
+}

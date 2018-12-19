f3ab1c0e0b297d113b47ad4116d51e43a286bf22
jaimemendozadev <jaimemendozadev@gmail.com>
2018-12-19 10:06:52 -0800

    Update dependencies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const app = require('./api')
 const server = require('./api/services/graphql')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)

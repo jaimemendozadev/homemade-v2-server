7a4aa659de1082d785285a6d820de20a0b5ab270
jaimemendozadev <jaimemendozadev@gmail.com>
2019-03-06 22:33:43 -0800

    Update dev dependencies

--- api/DB/index.js
+++ api/DB/index.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const {DB_URL} = process.env;
 
-mongoose.connect(
-  DB_URL,
-  {useNewUrlParser: true},
-);
+mongoose.connect(DB_URL, {useNewUrlParser: true});
 
 const DB = mongoose.connection;
 

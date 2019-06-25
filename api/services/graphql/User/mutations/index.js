c198ba05045c308c4c43f7bdeb471e668ec205bc
jaimemendozadev <jaimemendozadev@gmail.com>
2019-06-25 13:04:52 -0700

    Add createUser mutation to User gql schema

--- api/services/graphql/User/mutations/index.js
+++ api/services/graphql/User/mutations/index.js
@@ -1,6 +1,21 @@
 // parent, args, context, info
 
-const createUser = () => {};
+const createUser = async (_parent, {body}, {models}) => {
+  const userPayload = body;
+  const {User} = models;
+  const errorMsg = "There was an error saving the new user in the DB.";
+
+  try {
+    const NewUser = await User.create(userPayload);
+
+    return NewUser;
+
+  } catch(error) {
+    console.log(`${errorMsg} `, error);
+    throw new Error(errorMsg);
+  }
+
+};
 
 const updateUser = () => {};
 

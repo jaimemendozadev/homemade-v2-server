94679d0ac81d78a2e4aa561595b2c516555ca610
jaimemendozadev <jaimemendozadev@gmail.com>
2019-07-04 21:46:45 -0700

    Continue work on Order cart property query

--- api/services/graphql/Order/queries/index.js
+++ api/services/graphql/Order/queries/index.js
@@ -15,7 +15,6 @@ const getPendingOrders = async (_parent, {chefID}) => {
 const getUserCurrentOrder = async (_parent, {userID}) => {
   const errorMsg = 'The User has no current pending orders.';
 
-  console.log('inside getUserCurrentOrder');
   try {
     const pendingOrders = await findPendingOrders('User', userID);
 
@@ -31,7 +30,22 @@ const getCustomerOrders = () => {};
 const getAcceptedOrders = () => {};
 const getCompletedOrders = () => {};
 const getCancelledOrders = () => {};
-const cart = () => {};
+
+const cart = async ({_id}, _args, {models}) => {
+  const {Order} = models;
+  const errorMsg = 'Could find any dishes attached to the current order.';
+
+  try {
+    const foundOrder = await Order.findById(_id).populate('cart');
+    const {cart}= foundOrder;
+
+    return cart;
+    
+  } catch(error) {
+    console.log(`${errorMsg} `, error);
+    throw new Error(errorMsg);
+  }
+};
 
 module.exports = {
   getPendingOrders,

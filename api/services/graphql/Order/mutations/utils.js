d7c654a1c8f7808f071fccedf62ca1f04a549ac7
jaimemendozadev <jaimemendozadev@gmail.com>
2019-07-05 22:12:01 -0700

    Start work on refactoring postNewOrder mutation

--- api/services/graphql/Order/mutations/utils.js
+++ api/services/graphql/Order/mutations/utils.js
@@ -1,19 +1,17 @@
 const generateErrorMsg = statusCode => {
-    const errorMessages = {
-      1: 'to be accepted',
-      2: 'to be completed',
-      3: 'to be canceled',
-      4: 'to be reviewed',
-    };
+  const errorMessages = {
+    1: 'to be accepted',
+    2: 'to be completed',
+    3: 'to be canceled',
+    4: 'to be reviewed',
+  };
 
+  return `Could not update the order ${errorMessages[statusCode]} with status code ${statusCode}.`;
+};
 
-    return `Could not update the order ${errorMessages[statusCode]} with status code ${statusCode}.`;
-}
-
-const tabulateOrder = () => {}
-
+const tabulateOrder = () => {};
 
 module.exports = {
-    generateErrorMsg,
-    tabulateOrder
-}
+  generateErrorMsg,
+  tabulateOrder,
+};

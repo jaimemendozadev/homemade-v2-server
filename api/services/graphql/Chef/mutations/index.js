929535d3c19cad82e22e4f0bcd74b21f30f58a95
jaimemendozadev <jaimemendozadev@gmail.com>
2019-07-11 20:16:41 -0700

    Start work on postChefReview mutation

--- api/services/graphql/Chef/mutations/index.js
+++ api/services/graphql/Chef/mutations/index.js
@@ -1,4 +1,17 @@
-const postChefReview = () => {};
+/*
+const postChefReview = async (_parent, {chefID}, {models}) => {
+  const errorMsg = "Could not find reviews for this chef.";
+
+  try{
+  
+  } catch(error) {
+    console.log(`${errorMsg} ${error}`);
+    throw new Error(error);
+  }
+};
+*/
+
+const postChefReview = () => {}
 
 module.exports = {
   postChefReview,
